92250579b9620bec771d2d5eea0dfc45239fea85
hardick <bhadauriahardick@gmail.com>
2025-02-03 01:18:13 +0530

    idk6

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 // CORS configuration
 app.use(cors({
     origin: process.env.NODE_ENV === 'production' 
-        ? 'https://your-frontend-url.onrender.com' 
+        ? 'https://val-edpb.onrender.com/' 
         : 'http://localhost:5173',
     credentials: true
 }));
